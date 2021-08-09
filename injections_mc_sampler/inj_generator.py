2775e6e41f05d1634c5c12fcb7051603e0a949a9
jsantoro392 <48946331+jsantoro392@users.noreply.github.com>
2021-08-09 11:18:21 +0200

    update inj_generator.py with plots commands

--- injections_mc_sampler/inj_generator.py
+++ injections_mc_sampler/inj_generator.py
@@ -28,3 +28,50 @@ while(itr >= count):
         mass2.append(m2)
         Likelihood.append(likelihood)
         count = count + 1   
+
+# Histograms of masses distributions
+
+# Mass 1
+M1 = pd.DataFrame(mass1, columns = ['mass1'])
+figure =plt.subplots(figsize=(12,6))
+sns.histplot(data=M1, x="mass1", log_scale=True, color="purple", label = "mass1")
+plt.title('mass1', fontsize=16); 
+plt.xlabel("m1")
+plt.legend()
+plt.savefig('/home/giada.caneva/2_PBH/o2_mass1_histogram.png')
+plt.show()
+
+# Mass2
+M2 = pd.DataFrame(mass2, columns = ['mass2'])
+figure =plt.subplots(figsize=(12,6))
+sns.histplot(data=M2, x="mass2", log_scale=True, color="purple", label = "mass2")
+plt.title('mass2', fontsize=16); 
+plt.xlabel("m2")
+plt.legend()
+plt.savefig('/home/giada.caneva/2_PBH/o2_mass2_histogram.png')
+plt.show()
+
+# Lower mass gap region
+temp = []
+for i in range(len(mass1)):
+    if mass1[i]<2.5:
+        temp.append(mass1[i])
+        
+M1_temp = pd.DataFrame(temp, columns = ['mass1'])
+
+figure =plt.subplots(figsize=(12,6))
+#sns.histplot(data=M1_temp, x="mass1", log_scale=True, color="purple", label = "mass1")
+sns.distplot(M1_temp, color="purple", label="mass1")
+plt.title('mass1', fontsize=16); 
+plt.xlabel("m1")
+plt.legend()
+plt.savefig('/home/giada.caneva/2_PBH/o2_mass1_upto1.png')
+plt.show()
+
+# Histogram of likelihood distribution
+L = pd.DataFrame(Likelihood, columns = ['Likelihood'])
+figure =plt.subplots(figsize=(12,6))
+sns.histplot(data=L, x="Likelihood", log_scale=True, color="purple", label = "Likelihood O3")
+plt.title('Likelihood', fontsize=16); 
+plt.xlabel("L")
+plt.legend()
